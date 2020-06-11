cee979b3aae5162ac2db29523deee7576c2c5a55
EUROPE\artsoufl <Artemis.Tsouflidou@microsoft.com>
2020-06-11 15:25:30 +0100

    Add project files.

---
+++ CityInfoAPI/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        }
+    }
+
+}
