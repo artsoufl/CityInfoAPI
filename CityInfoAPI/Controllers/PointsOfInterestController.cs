09bbb56e1fd5f9a669d80c1d160a106b3e7fa7c3
EUROPE\artsoufl <Artemis.Tsouflidou@microsoft.com>
2020-06-19 13:40:57 +0100

    manipulating resources

--- CityInfoAPI/Controllers/PointsOfInterestController.cs
+++ CityInfoAPI/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfoAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +22,7 @@ namespace CityInfoAPI.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -31,5 +33,99 @@ namespace CityInfoAPI.Controllers
 
             return Ok(poi);
         }
+
+        [HttpPost]
+        public IActionResult CreatePointOfInterest(int cityid, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if (pointOfInterest == null) return BadRequest();
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityid);
+            if (city == null) return NotFound();
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p =>p.Id);
+
+            var finalPoi = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPoi);
+
+            // validating the input that way
+            return CreatedAtRoute(
+                "GetPointOfInterest", 
+                new { cityid = cityid, id = finalPoi.Id},
+                finalPoi);
+        }
+
+        [HttpPut("{id}")]    // id of the poi that we will update
+        public IActionResult UpdatePointOfInterest(int cityid, int id, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if (pointOfInterest == null) return BadRequest();
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityid);
+            if (city == null) return NotFound();
+
+            var poiFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (poiFromStore == null) return NotFound();
+
+            // in this way if in the new object description is null then it will be set to null
+            poiFromStore.Name = pointOfInterest.Name;
+            poiFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
+        }
+
+        // we need this in order to be able to update specific properties and not the whole object
+        // so we can update only the name or only the description
+        [HttpPatch("{id}")]    // id of the poi that we will update
+        public IActionResult PartiallyUpdatePointOfInterest(int cityid, int id, [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDoc)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityid);
+            if (city == null) return NotFound();
+
+            var poiFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (poiFromStore == null) return NotFound();
+
+            var pointOfInterestToPatch =
+                new PointOfInterestForUpdateDto()
+                {
+                    Name = poiFromStore.Name,
+                    Description = poiFromStore.Description
+                };
+
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (pointOfInterestToPatch.Description == pointOfInterestToPatch.Name)
+            {
+                ModelState.AddModelError("Description", "Description should be different from the name");
+            }
+
+            if (!TryValidateModel(pointOfInterestToPatch)) return BadRequest(ModelState);
+
+            poiFromStore.Name = pointOfInterestToPatch.Name;
+            poiFromStore.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null) return NotFound();
+
+            var poiFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (poiFromStore == null) return NotFound();
+
+            city.PointsOfInterest.Remove(poiFromStore);
+
+            return NoContent();
+        }
+
     }
 }
--- CityInfoAPI/Startup.cs
+++ CityInfoAPI/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfoAPI
 {
@@ -15,7 +17,17 @@ namespace CityInfoAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().
+                AddMvcOptions(o => o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter())); // in order to be able to support xml
+                // the followin will make the json object names to start with capital letter. serializer settings
+           // AddJsonOptions(o =>
+           //{
+           //    if (o.SerializerSettings.ContractResolver != null)
+           //    {
+           //        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+           //        castedResolver.NamingStrategy = null;
+           //    }
+           //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
