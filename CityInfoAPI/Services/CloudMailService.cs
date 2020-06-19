1f2b31285932383bd7043a34563139ad1156b8d3
EUROPE\artsoufl <Artemis.Tsouflidou@microsoft.com>
2020-06-19 15:21:12 +0100

    extend my mail service

--- CityInfoAPI/Services/CloudMailService.cs
+++ CityInfoAPI/Services/CloudMailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,12 +9,16 @@ namespace CityInfoAPI.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "atrsoufl@gmail.com";
-        private string _mailFrom = "atrsoufl@gmail.com";
+        private readonly IConfiguration _config;
+
+        public CloudMailService(IConfiguration config)
+        {
+            _config = config;
+        }
 
         public void Send(string subject, string message)
         {
-            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo} from CloudMailService");
+            Debug.WriteLine($"Mail from {_config["mailSettings:mailFromAddress"]} to {_config["mailSettings:mailToAddress"]}  from CloudMailService");
             Debug.WriteLine($"Subject {subject}");
             Debug.WriteLine($"Message {message}");
         }
--- CityInfoAPI/Services/LocalMailService.cs
+++ CityInfoAPI/Services/LocalMailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,12 +9,16 @@ namespace CityInfoAPI.Services
 {
     public class LocalMailService : IMailService
     {
-        private string _mailTo = "atrsoufl@gmail.com";
-        private string _mailFrom = "atrsoufl@gmail.com";
+        private readonly IConfiguration _config;
+
+        public LocalMailService(IConfiguration config)
+        {
+            _config = config;
+        }
 
         public void Send(string subject, string message)
         {
-            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo} from LocalMailService");
+            Debug.WriteLine($"Mail from {_config["mailSettings:mailFromAddress"]} to {_config["mailSettings:mailToAddress"]} from LocalMailService");
             Debug.WriteLine($"Subject {subject}");
             Debug.WriteLine($"Message {message}");
         }
